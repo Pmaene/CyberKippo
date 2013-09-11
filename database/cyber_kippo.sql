5acd21c9cbf70a60a10d880fc4a6725fcdc8972a
Pieter Maene <p.maene@gmail.com>
2013-09-11 14:18:41 +0200

    Finished integration with CIF

---
+++ database/cyber_kippo.sql
@@ -0,0 +1,106 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+CREATE DATABASE IF NOT EXISTS `cyber_kippo` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `cyber_kippo`;
+
+DROP TABLE IF EXISTS `auth`;
+CREATE TABLE IF NOT EXISTS `auth` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session` char(32) NOT NULL,
+  `success` tinyint(1) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+DROP TABLE IF EXISTS `cif`;
+CREATE TABLE IF NOT EXISTS `cif` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session` char(32) NOT NULL,
+  `purpose` varchar(50) DEFAULT NULL,
+  `asn` int(4) DEFAULT NULL,
+  `asn_desc` tinytext,
+  `portlist` text,
+  `rir` varchar(15) DEFAULT NULL,
+  `alternativeid` tinytext,
+  `alternativeid_restriction` varchar(50) DEFAULT NULL,
+  `cc` char(2) DEFAULT NULL,
+  `severity` varchar(50) DEFAULT NULL,
+  `assessment` varchar(50) DEFAULT NULL,
+  `description` tinytext,
+  `detecttime` datetime DEFAULT NULL,
+  `reporttime` datetime DEFAULT NULL,
+  `confidence` int(3) DEFAULT NULL,
+  `restriction` varchar(50) DEFAULT NULL,
+  `prefix` varchar(18) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2228 ;
+
+DROP TABLE IF EXISTS `clients`;
+CREATE TABLE IF NOT EXISTS `clients` (
+  `id` int(4) NOT NULL AUTO_INCREMENT,
+  `version` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+DROP TABLE IF EXISTS `downloads`;
+CREATE TABLE IF NOT EXISTS `downloads` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session` char(32) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `url` text NOT NULL,
+  `outfile` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `session` (`session`,`timestamp`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `input`;
+CREATE TABLE IF NOT EXISTS `input` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session` char(32) NOT NULL,
+  `timestamp` datetime NOT NULL,
+  `realm` varchar(50) DEFAULT NULL,
+  `success` tinyint(1) DEFAULT NULL,
+  `input` text NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `session` (`session`,`timestamp`,`realm`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `sensors`;
+CREATE TABLE IF NOT EXISTS `sensors` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `ip` varchar(15) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
+DROP TABLE IF EXISTS `sessions`;
+CREATE TABLE IF NOT EXISTS `sessions` (
+  `id` char(32) NOT NULL,
+  `starttime` datetime NOT NULL,
+  `endtime` datetime DEFAULT NULL,
+  `sensor` int(4) NOT NULL,
+  `ip` varchar(15) NOT NULL DEFAULT '',
+  `termsize` varchar(7) DEFAULT NULL,
+  `client` int(4) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `starttime` (`starttime`,`sensor`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `ttylog`;
+CREATE TABLE IF NOT EXISTS `ttylog` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `session` char(32) NOT NULL,
+  `ttylog` mediumblob NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- web/index.php
+++ web/index.php
@@ -13,14 +13,18 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
+$countries = include_once __DIR__ . '/countries.php';
+
 // Registering Service Providers
+$app->register(new Igorw\Silex\ConfigServiceProvider(__DIR__ . '/../config/parameters.json'));
+
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'db.options' => array(
-        'driver'    => 'pdo_mysql',
-        'host'      => 'localhost',
-        'dbname'    => 'cyber_kippo',
-        'user'      => 'cyber_kippo',
-        'password'  => '9hFDxWHSEWfmyLyK',
+        'driver'    => $app['parameters']['database']['driver'],
+        'host'      => $app['parameters']['database']['host'],
+        'dbname'    => $app['parameters']['database']['dbname'],
+        'user'      => $app['parameters']['database']['user'],
+        'password'  => $app['parameters']['database']['password'],
     ),
 ));
 
@@ -50,24 +54,100 @@ $app->before(function (Symfony\Component\HttpFoundation\Request $request) use ($
     $app['twig']->addGlobal('_route', $request->get('_route'));
 });
 
+// Helper Functions
+function _getSessions($app, $nbSessions = 5, $startSessions = 0) {
+    global $countries;
+
+    $sessions = $app['db']->fetchAll('SELECT id, ip, starttime, endtime FROM sessions ORDER BY starttime DESC LIMIT ' . $startSessions . ', ' . $nbSessions);
+    foreach ($sessions as $i => $session) {
+        $sessions[$i]['starttime'] = new \DateTime($session['starttime']);
+        $sessions[$i]['endtime'] = new \DateTime($session['endtime']);
+
+        $auth = $app['db']->fetchAll('SELECT success, username, password, timestamp FROM auth WHERE session = ? ORDER BY timestamp DESC', array($session['id']));
+        if (count($auth) > 0) {
+            $sessions[$i]['auth'] = $auth;
+            foreach ($sessions[$i]['auth'] as $j => $auth)
+                $sessions[$i]['auth'][$j]['timestamp'] = new \DateTime($auth['timestamp']);
+        }
+
+        $nbCif = $app['db']->fetchAssoc('SELECT COUNT(id) as nbCif FROM cif WHERE session = ? AND reporttime > DATE_SUB(NOW(), INTERVAL ? DAY)', array($session['id'], 5))['nbCif'];
+        if ($nbCif == 0) {
+            foreach (_queryCif($session['ip']) as $cif) {
+                $cif['detecttime'] = new \DateTime($cif['detecttime']);
+                $cif['reporttime'] = new \DateTime($cif['reporttime']);
+
+                $app['db']->insert(
+                    'cif',
+                    array(
+                        'session'                   => $session['id'],
+                        'purpose'                   => $cif['purpose'],
+                        'asn'                       => $cif['asn'],
+                        'asn_desc'                  => $cif['asn_desc'],
+                        'portlist'                  => isset($cif['portlist']) ? $cif['portlist'] : null,
+                        'rir'                       => $cif['rir'],
+                        'alternativeid'             => $cif['alternativeid'],
+                        'alternativeid_restriction' => $cif['alternativeid_restriction'],
+                        'cc'                        => $cif['cc'],
+                        'severity'                  => $cif['severity'],
+                        'assessment'                => $cif['assessment'],
+                        'description'               => $cif['description'],
+                        'detecttime'                => $cif['detecttime']->format('Y-m-d H:i:s'),
+                        'reporttime'                => $cif['reporttime']->format('Y-m-d H:i:s'),
+                        'confidence'                => $cif['confidence'],
+                        'restriction'               => $cif['restriction'],
+                        'prefix'                    => $cif['prefix'],
+                    )
+                );
+            }
+        }
+
+        $cif = $app['db']->fetchAll('SELECT * FROM cif WHERE session = ? AND confidence > ? AND reporttime > DATE_SUB(NOW(), INTERVAL ? DAY) GROUP BY alternativeid, detecttime ORDER BY detecttime DESC', array($session['id'], 80, 5));
+        if (count($cif) > 0) {
+            $sessions[$i]['cif'] = $cif;
+
+            foreach ($sessions[$i]['cif'] as $j => $cif) {
+                $sessions[$i]['cif'][$j]['detecttime'] = new \DateTime($cif['detecttime']);
+                $sessions[$i]['cif'][$j]['reporttime'] = new \DateTime($cif['reporttime']);
+
+                $sessions[$i]['cif'][$j]['country'] = htmlentities($countries[$cif['cc']]);
+            }
+        }
+    }
+
+    return $sessions;
+}
+
+function _queryCif($query) {
+    $client = new Buzz\Client\FileGetcontents();
+    $client->setTimeout(60);
+
+    $browser = new Buzz\Browser($client);
+    $response = $browser->get(
+        'http://' . $app['parameters']['cif']['host'] . '/api?apikey=' . $app['parameters']['cif']['api_key'] . '&q=' . $query,
+        array('User-Agent' => 'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.17 Safari/537.36')
+    );
+
+    $objects = explode("\n", $response->getContent());
+    foreach ($objects as $i => $object)
+        $objects[$i] = (array) json_decode($object);
+    return $objects;
+}
+
 // Controllers
 $app->get('/', function () use ($app) {
-    $sessions = $app['db']->fetchAll('SELECT id, ip, starttime, endtime FROM sessions ORDER BY starttime DESC LIMIT 5');
-
     return $app['twig']->render('index.twig', array(
-        'sessions' => $sessions
+        'sessions' => _getSessions($app)
     )); 
 })
 ->bind('index');
 
 $app->get('/history/page/{page}', function ($page) use ($app) {
     $nbPages = ceil(($app['db']->fetchAssoc('SELECT COUNT(id) AS nbPages FROM sessions')['nbPages'])/10);
-    $sessions = $app['db']->fetchAll('SELECT ip, starttime, endtime FROM sessions ORDER BY starttime DESC LIMIT ' . (($page-1)*10) . ', 10');
 
     return $app['twig']->render('history.twig', array(
         'nbPages' => $nbPages,
         'currentPage' => $page,
-        'sessions' => $sessions
+        'sessions' =>_getSessions($app, 10, ($page-1)*10)
     ));
 })
 ->bind('history');
